469ba0994bc92fcdd59ba9673c46cf8dbe69ed96
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-10-11 09:26:59 +0100

    Update on Linker/ setting up ganex search / setting up filter

--- git-sync.sh
+++ git-sync.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+declare -i i=1
+while true
+do
+	echo "_______________"$i"_________________"
+	git rm -rf --cached .
+	git add -A
+	git commit -am "Update on Linker/ setting up ganex search / setting up filter"
+	git push
+	echo "Hi, bm7. A update has been done :)"
+	i=$i+1
+	echo "____________________________________"
+	sleep 15
+done
 
-
-echo "________________________________"
-git rm -rf --cached .
-git add -A
-git commit -am "Updated Vpot / $1"
-#git push origin master
-echo "Hi, bm7. A update has been done :)"
-echo "____________________________________"
